205bd0933790ba060f4c016fd5ffee3a43fdbc8d
knr2003 <114396360+knr2003@users.noreply.github.com>
2023-04-04 00:02:13 +0530

    Add files via upload

---
+++ 03-04-2023/Pattern8.java
@@ -0,0 +1,14 @@
+class Pattern8{
+	public static void main(String args[]){
+		int row=4;
+		for(int i=1;i<=4;i++){
+			for(int k=3;k>=i;k--){
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
